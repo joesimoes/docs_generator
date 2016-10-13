c7cf597626d122ff3839d74b49fc4852e6280466
Joe Simoes <joseph.simoes@gmail.com>
2016-10-13 16:52:36 -0400

    Set up user auth

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var express = require('express');
 express()
   .set('view engine', 'ejs')
   .use(express.static('./public'))
+  .use(require('./accounts'))
   .get('*', function (req, res) {
     res.render('index', {
-      user: null
+      user: JSON.stringify(req.session.user || null)
     });
   })
   .listen(3000)
