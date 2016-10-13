da2686fb464bd0ec62fb6a088ba92bc339082b2a
Joe Simoes <joseph.simoes@gmail.com>
2016-10-12 22:58:08 -0400

    Set up server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+express()
+  .set('view engine', 'ejs')
+  .use(express.static('./public'))
+  .get('*', function (req, res) {
+    res.render('index', {
+      user: null
+    });
+  })
+  .listen(3000)
