bbb0f405802c21b56fb4820dec812ae7400b6c26
Joe Simoes <joseph.simoes@gmail.com>
2016-10-17 23:36:18 -0400

    Router and package setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Router from 'react-router';
+import App from './components/App';
+
+const {Route} = Router;
+
+var routes = <Route handler={App}>
+
+</Route>;
+Router.run(routes, Router.HistoryLocation, Root => 
+    React.render(<Root />, documnet.getElementById('app')));
+
+
